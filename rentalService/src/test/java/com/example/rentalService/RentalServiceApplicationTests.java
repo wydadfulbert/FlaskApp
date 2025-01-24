117d8a7ee70a88e209d82268ffd918caf7b39bb1
charroux <benoit.charroux@gmail.com>
2025-01-24 09:40:24 +0100

    update action with Docker

--- rentalService/src/test/java/com/example/rentalService/RentalServiceApplicationTests.java
+++ rentalService/src/test/java/com/example/rentalService/RentalServiceApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.rentalService;
 
+import com.example.rentalService.data.Car;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class RentalServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void test1(){
+		Car car = new Car("11AA22", 1000);
+		assertEquals(car.getPlateNumber(), "11AA22");
 	}
 
 }
